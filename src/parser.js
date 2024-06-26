7cc825b225afdf451e2e7b69e29837f565ff1775
Dima Myagkov <dimon.mykish@gmail.com>
2024-06-26 18:36:45 +0300

    fixing linter errors

--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,16 @@
 import ymal from 'js-yaml';
 
 const parser = (data, format) => {
-    switch (format) {
-        case 'json':
-            return JSON.parse(data);
-        case 'yml':
-            return ymal.load(data);
-        case 'yaml':
-            return ymal.load(data);
-        default:
-            throw new Error(`Invalid format: ${format}`);
-        }
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+       return ymal.load(data);
+    case 'yaml':
+       return ymal.load(data);
+    default:
+       throw new Error(`Invalid format: ${format}`);
+  }
 };
 
 export default parser;
